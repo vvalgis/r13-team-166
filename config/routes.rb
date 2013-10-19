80139aa7a591c40e2081e08744c7785df43b89a2
Konstantin Akhantev <saviorisdead@gmail.com>
2013-10-20 00:17:04 +0400

    added route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Scriba::Application.routes.draw do
 
-  get "home/index"
+  get "/" => 'home#index', as: :index
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
--- db/schema.rb
+++ db/schema.rb
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20131019145852) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "guest"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
