dca45a30563f0085bd78b565c4cb081ee9b49a95
Konstantin Akhantev <saviorisdead@gmail.com>
2013-10-19 20:27:52 +0400

    wip

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,12 @@ class UsersController < ApplicationController
   end
 
   def create
-    
+    #@user = params[:user] ? User.new(params[:user]) : User.new_quest
+#    if @user.save
+#      session[:user_id] = @user.id
+#      redirect_to root_url
+#    else
+#      redirect_to new_user_registration_path
+#    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Scriba::Application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'projects#index'
   resources :users
-  get '/tryout' => 'users#tryout', as: :tryout
   resources :projects
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
