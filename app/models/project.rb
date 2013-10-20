5a36ce5f9dbf199bda0d11aeeb4219ee88af987a
Vladimir Valgis <vladimir.valgis@gmail.com>
2013-10-21 03:22:21 +0400

    Fix project edit

--- app/models/project.rb
+++ app/models/project.rb
@@ -4,12 +4,22 @@ class Project < ActiveRecord::Base
   attr_accessor :text
 
   def original
-    documents.joins(:self_edition).where(self_edition: {parent_id: nil}).first
+    documents.select('"documents".*').joins(:self_edition).where(editions: {parent_id: nil}).first
+  end
+
+  def text
+    original ? original.text.to_s : @text.to_s
   end
 
   after_create do
     doc = documents.build(comment: 'original', lang: lang)
     doc.text = text
+    doc.save
+  end
+
+  after_update do
+    doc = original
+    doc.text = @text
     doc.save
   end
 end
